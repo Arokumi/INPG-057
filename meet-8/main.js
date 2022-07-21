4e120add23547995b7246119812a7e062d352a50
Arokumi <aamsai16@gmail.com>
2022-07-21 21:30:56 +0700

    session 8

---
+++ meet-8/main.js
@@ -0,0 +1,46 @@
+//Declaring Objects and key:value
+
+// method 1
+let obj = {
+    name: "Aro"
+};
+
+
+// method 2
+obj["age"] = 15;
+
+// method 3
+obj.gender = 'male';
+
+// console.log(obj)
+
+// Practice
+
+function showBond() {
+    console.log("Sylvie")
+}
+
+let arthurObj = {
+    name: 'Arthur Leywin',
+    otherAliases: [
+        'Art',
+        'Grey',
+        'Godspell'
+    ],
+    deviants: {
+        power_1: 'Lightning',
+        power_2: 'Ice',
+        power_3: 'Gravity',
+    },
+    statusAlive: true,
+    gender: 'male',
+    age: 17,
+    birthday: 'May 29',
+    bond: showBond,
+};
+
+
+//WTF IS LET FOR (IN) I AM CONFUSED HELP IM GONNA DIE
+
+
+
