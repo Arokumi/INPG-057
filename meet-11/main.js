ebf0d044aaf76e0c8625d3ffd99c832e217973fe
Arokumi <aamsai16@gmail.com>
2022-08-02 20:55:51 +0700

    session 11

---
+++ meet-11/main.js
@@ -0,0 +1,10 @@
+let text = document.getElementsByClassName("shriek");
+let newText = document.createTextNode("Mushroom ");
+
+text[0].prepend(newText)
+
+let p = document.getElementById("creak")
+let pText = document.createElement("m");
+pText.innerText = "Tomato "
+
+p.prepend(pText);
