06dba5342d798fe229e68f3b980dcd79ba2eb653
Arokumi <aamsai16@gmail.com>
2022-08-09 22:11:46 +0700

    Session 12

---
+++ meet-12/main.js
@@ -0,0 +1,25 @@
+let pattern1 = /[mrlh]/gi;
+let text1 = "Arhamuzzaky Ibrahim Alfa";
+let check1 = pattern1.test(text1);
+let find1 = text1.search(pattern1);
+
+// console.log(result);
+
+function result1() {
+    console.log(check1);
+    console.log(find1);
+} ;
+
+result1()
+
+// let pattern2 = /[aiueo]{2, 100}/gi;
+// let text2 = "Muhammad Ihsan";
+// let check2 = pattern2.test(text2);
+// let find2 = text2.search(pattern2);
+
+// function result2() {
+//     console.log(check2);
+//     console.log(find2);
+// } ;
+
+// result2()
